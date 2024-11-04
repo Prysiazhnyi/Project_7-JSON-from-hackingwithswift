080659857d2bce8d31ec224103e446d34bf4e57c
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-04 20:48:45 +0200

    add-step_to_finish

---
+++ Project-7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Project-7
+//
+//  Created by Serhii Prysiazhnyi on 04.11.2024.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project-7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project-7
+//
+//  Created by Serhii Prysiazhnyi on 04.11.2024.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
