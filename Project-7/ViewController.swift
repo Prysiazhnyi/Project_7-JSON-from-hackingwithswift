c057b0cbcc9c7e15e4d3571fb7241ea5e1fd4556
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-06 21:56:09 +0200

    add-async

--- Project-7/ViewController.swift
+++ Project-7/ViewController.swift
@@ -30,13 +30,19 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parse(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self.parse(json: data)
+                    return
+                }
+            }
+            DispatchQueue.main.async {
+                self.showError()
+                self.tableView.reloadData()
             }
         }
-        showError()
+        tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -56,7 +62,10 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
     
@@ -67,9 +76,11 @@ class ViewController: UITableViewController {
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
     }
     
     @objc func showCredits() {
@@ -93,11 +104,11 @@ class ViewController: UITableViewController {
     }
     
     func search(for text: String) {
-    
+        
         filteredPetitions = petitions.filter { $0.title.lowercased().contains(text.lowercased())}
         isFiltering = !filteredPetitions.isEmpty
         tableView.reloadData()
-    
+        
         if filteredPetitions.isEmpty {
             showAlert(title: "No results", message: "No petitions match your search.")
         }
